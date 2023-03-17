9a228314e1b04038e8e7ebe097b66b3e83d2df4d
Soccus <demobilli@mail.ru>
2023-03-17 14:20:19 +0500

    v1.1 - added attempt reset marking

---
+++ src/components/Reset.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './componentStyles.module.scss'
+
+function Reset({resetTable}) {
+  return (
+    <div className={styles.reset} onClick={resetTable}>
+      Reset
+    </div>
+  )
+}
+
+export default Reset
